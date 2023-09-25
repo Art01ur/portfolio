e89ac62ca4d275efd641aca7a62d36fe02c6e788
Art01ur <bakuntsartur93@gmail.com>
2023-09-25 22:30:08 +0400

    add new programmer html and view

--- programmers/urls.py
+++ programmers/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('programmer/<int:programmer_id>/', views.programmer_full_information, name='programmer_full_information'),
-    path('new/programmer', views.new_programmer, name='new_programmer')
+    path('new/programmer', views.new_programmer, name='new_programmer'),
+    path('new/programmer/data',views.new_programmer_data,name='new_programmer_data')
 
 ]
--- programmers/views.py
+++ programmers/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 from .models import Programmer
 
@@ -17,9 +18,35 @@ def programmer_full_information(request, programmer_id):
                   {'programmer': programmer, 'projects': projects})
 
 
-
 def new_programmer(request):
     return render(request, 'programmers/add_new_programmer.html', )
+
+
+def new_programmer_data(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        age = request.POST.get('age')
+        language = request.POST.get('language')
+        framework = request.POST.get('framework')
+        experience = request.POST.get('experience')
+
+        user = User.objects.create_user(
+            username=first_name,
+            password='aaa12345',
+            first_name=first_name,
+            last_name=last_name
+        )
+
+        programmer = Programmer.objects.create(
+            user=user,
+            age=age,
+            language=language,
+            framework=framework,
+            experience=experience
+        )
+        return redirect('home')
+
 
 def page_not_found_404(request, exception):
     return render(request, 'programmers/page_not_found.html', status=404)
